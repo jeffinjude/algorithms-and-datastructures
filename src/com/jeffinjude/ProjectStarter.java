09092b298be1580843fd323f74990f7629c99949
jeffinjude <jeffinjude@gmail.com>
2020-01-26 21:09:08 +0530

    Factorial using recursion

--- src/com/jeffinjude/ProjectStarter.java
+++ src/com/jeffinjude/ProjectStarter.java
@@ -1,12 +1,15 @@
 package com.jeffinjude;
 
+import com.jeffinjude.recursion.Factorial;
+
 public class ProjectStarter {
 	
 	/*
 	 * Main method where you give the function calls to execute.
 	 */
 	public static void main(String[] args) {
-
+		//Factorial algorithm
+		System.out.println(Factorial.calcFactorial(-10));
 	}
 
 }
--- src/com/jeffinjude/recursion/Factorial.java
+++ src/com/jeffinjude/recursion/Factorial.java
@@ -1,5 +1,17 @@
 package com.jeffinjude.recursion;
 
 public class Factorial {
-
+	
+	public static int calcFactorial(int n) {
+		if(n == 0) {
+			return 1;
+		}
+		else if(n < 0) {
+			System.err.println("Please enter a positive number.");
+			return 0;
+		}
+		else {
+			return (n*calcFactorial(n-1));
+		}
+	}
 }
